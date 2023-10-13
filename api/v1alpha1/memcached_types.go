e65334ed2fafd9ecf315c8492be1a51641bd3115
abhijeetdandekar333 <root@DESKTOP-F19E1SQ>
2023-10-13 18:16:29 +0530

    Updated api

--- api/v1alpha1/memcached_types.go
+++ api/v1alpha1/memcached_types.go
@@ -30,6 +30,8 @@ type MemcachedSpec struct {
 
 	// Foo is an example field of Memcached. Edit memcached_types.go to remove/update
 	Foo  string `json:"foo,omitempty"`
+
+	// Size is the size of memcached deployment
 	Size int32  `json:"size"`
 }
 
@@ -37,6 +39,7 @@ type MemcachedSpec struct {
 type MemcachedStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	// Nodes are the names of the memcached pods
 	Nodes []string `json:"nodes"`
 }
 
